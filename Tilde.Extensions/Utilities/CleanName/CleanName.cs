7d8ef3eefc6bdf62e391fcaa8e8b329edf3f6dce
xiaohanLiu2022 <104401857+xiaohanLiu2022@users.noreply.github.com>
2022-11-28 12:49:41 +1300

    Added clean name method

---
+++ Tilde.Extensions/Utilities/CleanName/CleanName.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Tilde.Extensions.Utilities
+{
+    public static class CleanName
+    {
+        public static string Clean(string name)
+        {
+            name = name.ToLower();
+            name = RemoveDiacritics(name);
+            name = RemoveDash(name);
+            name = RemoveDot(name);
+            name = RemoveSingleQuote(name);
+            name = RemoveTitleAndSuffix(name);
+            return name.Trim();
+        }
+
+        private static string RemoveDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder(capacity: normalizedString.Length);
+
+            for (int i = 0; i < normalizedString.Length; i++)
+            {
+                char c = normalizedString[i];
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder
+                .ToString()
+                .Normalize(NormalizationForm.FormC);
+        }
+
+        private static string RemoveDash(string text)
+        {
+            return text.Replace("-", "");
+        }
+
+        private static string RemoveDot(string text)
+        {
+            return text.Replace(".", "");
+        }
+
+        private static string RemoveSingleQuote(string text)
+        {
+            return text.Replace("\'", "").Replace("’", "");
+        }
+
+        private static string RemoveTitleAndSuffix(string text)
+        {
+            List<string> suffixList = new List<string> { "dr", "jr", "sr", "iii", "ii", "iv", "dame" };
+            foreach (string suffix in suffixList)
+            {
+                if (text.Contains(string.Format("{0} ", suffix)) || text.Contains(string.Format(" {0}", suffix)))
+                {
+                    text = text.Replace(suffix, "").Trim();
+                }
+            }
+            return text;
+        }
+    }
+}
