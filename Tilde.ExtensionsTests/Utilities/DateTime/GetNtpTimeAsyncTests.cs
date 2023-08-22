7bdb65c3afc4f01f4a3cbad741b464ca07cf1ede
SoftPuppet <softpuppet@users.noreply.github.com>
2023-08-23 00:17:16 +1200

    Create GetNtpTimeAsyncTests.cs

---
+++ Tilde.ExtensionsTests/Utilities/DateTime/GetNtpTimeAsyncTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tilde.Utilities;
+
+namespace Tilde.ExtensionsTests.Utilities;
+
+[TestClass]
+public class GetNtpTimeAsyncTests
+{
+    [TestMethod]
+    public async Task GetNtpTimeAsync_Successful_ReturnsValidDateTime()
+    {
+        // Arrange
+        string ntpServer = "time.cloudflare.com";
+        int ntpPort = 123;
+
+        // Act
+        var result = await DateTimeHelper.GetNtpTimeAsync(ntpServer, ntpPort);
+
+        // Assert
+        Assert.IsTrue(result.IsSuccessful);
+        Assert.IsNotNull(result.DateTime);
+        Assert.AreNotEqual(result.DateTime, new DateTime());
+        Assert.AreEqual(result.ErrorMessage, string.Empty);
+    }
+
+    [TestMethod]
+    public async Task GetNtpTimeAsync_InvalidServer_ReturnsErrorMessage()
+    {
+        // Arrange
+        string ntpServer = "invalid.server_askdfjk.com";
+        int ntpPort = 123;
+
+        // Act
+        var result = await DateTimeHelper.GetNtpTimeAsync(ntpServer, ntpPort);
+
+        // Assert
+        Assert.IsFalse(result.IsSuccessful);
+        Assert.AreEqual(result.DateTime, new DateTime());
+        Assert.IsNotNull(result.ErrorMessage);
+    }
+}
