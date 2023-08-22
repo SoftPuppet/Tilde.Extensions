25370ad99b971601644297c1008783c7b17cdce6
SoftPuppet <softpuppet@users.noreply.github.com>
2023-08-23 00:14:33 +1200

    Create Enums.cs

---
+++ Tilde.Extensions/Enums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tilde.Extensions
+{
+    // Enum for the RemovalMode used in Tilde.Extensions.Strings.RemoveOccurences(this string source, RemovalMode mode ...)
+    public enum RemovalMode
+    {
+        Anywhere,
+        StartOnly,
+        EndOnly
+    }
+
+    // Enum for GetFinancialYear(this DateTime source, FinancialYearStartMonth startMonth ...)
+    public enum Month
+    {
+        January = 1,
+        February = 2,
+        March = 3,
+        April = 4,
+        May = 5,
+        June = 6,
+        July = 7,
+        August = 8,
+        September = 9,
+        October = 10,
+        November = 11,
+        December = 12
+    }
+}
