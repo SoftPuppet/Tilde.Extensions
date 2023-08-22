a665bfbef33d0c20182a95ace45ac277b2970544
SoftPuppet <softpuppet@users.noreply.github.com>
2023-08-22 21:23:50 +1200

    Create Enums.cs

---
+++ Tilde.Extensions/Strings/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tilde.Extensions.Strings
+{
+    public enum RemovalMode
+    {
+        Anywhere,
+        StartOnly,
+        EndOnly
+    }
+}
