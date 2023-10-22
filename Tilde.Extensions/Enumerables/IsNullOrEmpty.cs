afae267eff4a9b162c2bb2518b5ec1a1bb27d406
SoftPuppet <softpuppet@users.noreply.github.com>
2023-10-22 19:09:47 +1300

    Create IsNullOrEmpty.cs

---
+++ Tilde.Extensions/Enumerables/IsNullOrEmpty.cs
@@ -0,0 +1,5 @@
+﻿namespace Tilde.Extensions;
+
+public static partial class EnumerableExtensions {
+  public static bool IsNullOrEmpty<T>(this IEnumerable<T>? source) => source is null || !source.Any();
+}
