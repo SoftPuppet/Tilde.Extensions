a4ce7da9d566770ddf10fbc293bd9f61c5747fa6
SoftPuppet <softpuppet@users.noreply.github.com>
2023-10-22 19:33:14 +1300

    Add usage example

--- Tilde.Extensions/Enumerables/IsNullOrEmpty.cs
+++ Tilde.Extensions/Enumerables/IsNullOrEmpty.cs
@@ -3,3 +3,9 @@
 public static partial class EnumerableExtensions {
   public static bool IsNullOrEmpty<T>(this IEnumerable<T>? source) => source is null || !source.Any();
 }
+// Example Usage
+/*
+var isEmpty = Enumerable.Empty<int>().IsNullOrEmpty(); // true
+var isEmpty = new[] { 1, 2, 3 }.IsNullOrEmpty(); // false
+var isEmpty = ((IEnumerable<int>)null).IsNullOrEmpty(); // true
+*/
